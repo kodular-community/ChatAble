d314471c1b69ee2b39782b928ab8e96125e07d6c
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-07-07 14:04:50 +0200

    Improved demo

--- utils/translate/index.php
+++ utils/translate/index.php
@@ -1,10 +1,19 @@
 <?php
 
-require 'autoload.php';
-
-use Stichoza\GoogleTranslate\TranslateClient;
-
-$tr = new TranslateClient();
-$tr->setTarget($_GET['lang']);
-
-echo $tr->translate('Hello World!');
+if ($_GET['mode'] == 'demo') {
+  $tr = new TranslateClient();
+  $tr->setTarget($_GET['lang']);
+  echo $tr->translate('Hello World!');
+} else {
+  $langs = json_decode(file_get_contents("gtlanguages.json"), true);
+  $numItems = count($langs);
+  $i = 0;
+  foreach ($langs as $lang) {
+    $i = $i+1;
+    if ($i == $numItems) {
+      echo $lang['language'];
+    } else {
+      echo $lang['language']."<br>";
+    }
+  }
+}
